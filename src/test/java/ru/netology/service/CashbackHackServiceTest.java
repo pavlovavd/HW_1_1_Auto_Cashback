aa3e9f24eb764410e202057f5ffb6f22d476280f
Victoria Pavlova <pvd0510@mail.ru>
2022-03-31 18:26:58 +0300

    Add CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,8 @@
 package ru.netology.service;
 
+import org.apiguardian.api.API;
+import org.junit.platform.commons.annotation.Testable;
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -29,6 +32,40 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void ReturnOfTheAmount0() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+
+
+    @Testable
+    public void APIReturnOfTheAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Testable
+    public void APIReturnOfTheAmount999() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Testable
+    public void APIReturnOfTheAmount1001() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1001);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Testable
+    public void APIReturnOfTheAmount0() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(0);
         int expected = 1000;
