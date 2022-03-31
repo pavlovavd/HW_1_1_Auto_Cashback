65e87fcbe790c7da4e504e737e88cdc372a8eb2f
Victoria Pavlova <pvd0510@mail.ru>
2022-03-31 18:07:51 +0300

    Add CashbackHackService.java

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void ReturnOfTheAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void ReturnOfTheAmount999() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void ReturnOfTheAmount1001() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1001);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void ReturnOfTheAmount0() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+}
